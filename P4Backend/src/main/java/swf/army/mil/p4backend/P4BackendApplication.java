c7a7cc52fd1d5e2f2544795a1ff35db4083516fe
justice chimara <jchimara1@gmail.com>
2025-04-28 14:21:09 -0500

    backend configured flyway entity created

---
+++ P4Backend/src/main/java/swf/army/mil/p4backend/P4BackendApplication.java
@@ -0,0 +1,13 @@
+package swf.army.mil.p4backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P4BackendApplication.class, args);
+	}
+
+}
---
+++ P4Backend/src/test/java/swf/army/mil/p4backend/P4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package swf.army.mil.p4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
